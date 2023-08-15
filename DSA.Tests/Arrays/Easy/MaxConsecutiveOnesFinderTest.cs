c0288ff797dcd38dcce1e179ac059022802811e9
Ravindra Kore <ravikank@gmail.com>
2023-08-15 21:43:01 +0530

    Add project files.

---
+++ DSA.Tests/Arrays/Easy/MaxConsecutiveOnesFinderTest.cs
@@ -0,0 +1,27 @@
+﻿using DSA.Arrays.Easy;
+
+namespace DSA.Tests.Arrays.Easy
+{
+    public class MaxConsecutiveOnesFinderTest
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 0, 1, 1, 1 }, 3)]
+        [InlineData(new[] { 1, 0, 1, 1, 0, 1 }, 2)]
+        [InlineData(new[] { 0, 0, 0, 0, 0, 0 }, 0)]
+        [InlineData(new[] { 1, 1, 1, 1, 1, 1 }, 6)]
+        [InlineData(new[] { 1, 1, 1, 0, 1, 1 }, 3)]
+        [InlineData(new[] { 1, 1, 1, 0, 1, 1, 1 }, 3)]
+        [InlineData(new[] { 1, 1, 1, 0, 1, 1, 1, 1 }, 4)]
+        [InlineData(new[] { 1, 1, 1, 0, 1, 1, 1, 1, 1 }, 5)]
+        [InlineData(new[] { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 }, 6)]
+        public void GivenValidArray_Find_ShouldReturnMaxConsecutiveOnes(int[] nums, int expected)
+        {
+            // Arrange
+            // Act
+            var actual = MaxConsecutiveOnesFinder.Find(nums);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DSA/Arrays/Easy/Max Consecutive Ones/MaxConsecutiveOnesFinder.cs
@@ -0,0 +1,32 @@
+﻿namespace DSA.Arrays.Easy
+{
+    public class MaxConsecutiveOnesFinder
+    {
+        /// <summary>
+        /// Finds the maximum number of consecutive 1s in the given array.
+        /// Time Complexity: O(n)
+        /// Space Complexity: O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns>maximum number of consecutive 1s in the given array.</returns>
+        public static int Find(int[] nums)
+        {
+            int best = 0;
+            int current = 0;
+
+            foreach (var num in nums)
+            {
+                if (num == 1)
+                {
+                    current++;
+                }
+                else
+                {
+                    best = Math.Max(best, current);
+                    current = 0;
+                }
+            }
+            return Math.Max(best, current);
+        }
+    }
+}
