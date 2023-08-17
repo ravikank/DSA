194c686940712ec5e080b4f17ad84c440853b82b
Ravindra Kore <ravikank@gmail.com>
2023-08-17 18:33:54 +0530

    Solved : Duplicate zeros problem

---
+++ DSA.Tests/Arrays/Easy/ZeroDuplicatorTest.cs
@@ -0,0 +1,25 @@
+﻿using DSA.Arrays.Easy;
+
+namespace DSA.Tests.Arrays.Easy
+{
+    public class ZeroDuplicatorTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 0, 2, 3, 0, 4, 5, 0 }, new int[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 })]
+        [InlineData(new int[] { 1, 0, 0, 2, 3, 0, 4, 5, 0 }, new int[] { 1, 0, 0, 0, 0, 2, 3, 0, 0 })]
+        [InlineData(new int[] { 1, 0, 2, 3, 0, 4, 5, 0, 6 }, new int[] { 1, 0, 0, 2, 3, 0, 0, 4, 5 })]
+        public void GiveArrayWithZeros_ShouldDuplicateZeros(int[] numbers, int[] expected)
+        {
+            // Arrange
+            var duplicator = new ZeroDuplicator();
+
+            // Act
+            var actual = duplicator.Duplicate(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DSA/Arrays/Easy/Duplicate Zeros/ZeroDuplicator.cs
@@ -0,0 +1,21 @@
+﻿namespace DSA.Arrays.Easy
+{
+    public class ZeroDuplicator
+    {
+        public int[] Duplicate(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    for (int j = numbers.Length - 1; j > i; j--)
+                    {
+                        numbers[j] = numbers[j - 1];
+                    }
+                    i++;
+                }
+            }
+            return numbers;
+        }
+    }
+}
