8888963698bf63f1991b9dcfa0a16ce73f5f140c
Ravindra Kore <ravikank@gmail.com>
2023-08-19 19:47:14 +0530

    Solved: Check If N and Its Double Exist problem

---
+++ DSA/Arrays/Easy/Check If N and Its Double Exist/NAndItsDoubleChecker.cs
@@ -0,0 +1,18 @@
+﻿namespace DSA.Arrays.Easy
+{
+    public class NAndItsDoubleChecker
+    {
+        public static bool CheckIfExist(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j] * 2 || numbers[j] == numbers[i] * 2)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
